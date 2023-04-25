d2dc1608b9acc9c516f71ccae087624d8acd9e4d
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-04-25 10:21:49 +0200

    api fecth start

---
+++ App_reco/src/routes/about/+page.svelte
@@ -0,0 +1,14 @@
+<!-- test with movie db -->
+<script context="module">
+    export async function load({ fetch }){
+        const res = await fetch(
+            'https://api.themoviedb.org/3/movie/550?api_key=0f19c042aac4b96dd9c50c70ff491332'
+        );
+        const data = await res.json();
+        if (res.ok){
+            return {
+                props: { popular: data.results }
+            };
+        }
+    }
+</script>
